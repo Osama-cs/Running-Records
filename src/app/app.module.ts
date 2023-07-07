3f5d0a0d89d437e80deae44fd6cbf7f79f4768d7
Osama <oilyascs@gmail.com>
2023-07-07 18:11:47 +0100

    working on basic form for signup page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module'
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CurrentPlantComponent } from './dashboard/current-plant/current-plant.component';
+import { PastPlantsComponent } from './dashboard/past-plants/past-plants.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [		
     AppComponent,
-      LandingPageComponent
+      LandingPageComponent,
+      SignupComponent,
+      LoginComponent,
+      CurrentPlantComponent,
+      PastPlantsComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
